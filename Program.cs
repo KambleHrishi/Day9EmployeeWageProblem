d8fa6b7c368d091c16a3ac6ee624005eb322b8ec
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-13 21:34:54 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day9EmployeeWageProblem
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Problem");
+        }
+    }
+}
